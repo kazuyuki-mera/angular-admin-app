20655600f486a84f9c41a83ca4d8e9f3a97cafcc
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-17 00:10:00 +0900

    社員データ編集可能にする

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MembersComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MembersComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
