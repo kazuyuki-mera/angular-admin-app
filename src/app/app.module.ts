b4e6a4a9e68590b29a4c361bfe4305797c2e5145
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-19 20:47:09 +0900

    メッセージログを表示する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, MembersComponent, MemberDetailComponent],
+  declarations: [AppComponent, MembersComponent, MemberDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from './member';
+import { MessageService } from './message.service';
 import { MEMBERS } from './mock-members';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MemberService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getMembers(): Observable<Member[]> {
+    this.messageService.add('MemberService: 社員一覧データを取得しました');
     return of(MEMBERS);
   }
 }
